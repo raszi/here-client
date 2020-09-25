bab6d39f270d9ed4713d4e4b3bf902e98963a702
KARASZI István <github@spam.raszi.hu>
2020-09-25 12:08:52 +0200

    Add missing utils

---
+++ src/utils.ts
@@ -0,0 +1,8 @@
+export const isNumber = (x: unknown): x is number => typeof x === 'number';
+export const isString = (x: unknown): x is string => typeof x === 'string';
+export const isBoolean = (x: unknown): x is boolean => typeof x === 'boolean';
+export const isArray = (x: unknown): x is unknown[] => Array.isArray(x);
+
+export const isRecord = (value: unknown): value is Record<string, unknown> => {
+  return value !== null && value !== undefined ? Object.getPrototypeOf(value).isPrototypeOf(Object) : false;
+};
