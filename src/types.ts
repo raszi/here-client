bc49c3a51b74a56b4ea0cd90e58ccd47fd90515d
KARASZI István <github@spam.raszi.hu>
2020-09-28 15:15:06 +0200

    Move out the src/types/index.ts to src/types.ts

---
+++ src/types.ts
@@ -0,0 +1,29 @@
+import DailyForecastType from './types/DailyForecastType';
+import DailyForecasts from './types/DailyForecasts';
+import ForecastLocationType from './types/ForecastLocationType';
+import ForecastType from './types/ForecastType';
+import Forecasts from './types/Forecasts';
+import HourlyForecastType from './types/HourlyForecastType';
+import HourlyForecasts from './types/HourlyForecasts';
+import LocationMetadata from './types/LocationMetadata';
+import LocationType from './types/LocationType';
+import ObservationType from './types/ObservationType';
+import Observations from './types/Observations';
+import WeatherItemsType from './types/WeatherItemsType';
+import WeatherReportType from './types/WeatherReportType';
+
+export {
+  DailyForecastType,
+  DailyForecasts,
+  ForecastLocationType,
+  ForecastType,
+  Forecasts,
+  HourlyForecastType,
+  HourlyForecasts,
+  LocationMetadata,
+  LocationType,
+  ObservationType,
+  Observations,
+  WeatherItemsType,
+  WeatherReportType
+};
